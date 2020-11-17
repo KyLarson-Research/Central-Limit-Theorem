9416bb774d9362f865f891e78f39fde5253a167b
admin <kylel@email.arizona.edu>
2020-11-17 08:58:48 -0700

    updated and finally working clt.py for demonstrating central limit theorem

--- clt.py
+++ clt.py
@@ -4,9 +4,7 @@
 import numpy as np
 import matplotlib as mpl
 from matplotlib import pyplot as plt
-x = np.arange(0, 5, 0.1);
-y = np.sin(x)
-plt.plot(x, y)
+
 #note to self install pandas as soon as possible
 #help(np.random)
 
@@ -16,10 +14,10 @@ plt.plot(x, y)
 #in a matrix of 900 elements
 #perform a mean of every row
 #plot each of the means in a histogram
-#
-#Demonstrate the face splitting product
-#
-#Calculate the number of weeks in 100 days
-#rds = np.random.rand(300, 300)
-#print(rds)
-#print(np.random(1))
+x = []
+rds = np.random.rand(300, 300)
+for i in range(len(rds[0])):
+	x.append(np.mean(rds[i]))
+
+plt.hist(x)
+plt.show()
